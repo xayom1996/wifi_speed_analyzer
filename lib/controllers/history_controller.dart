00623de33ac70cd0b4358b5ecc2d2df3a71ee562
xayom1996 <xayom1996@gmail.com>
2021-09-22 18:57:43 +0300

    add ISP, Server. History Page done 80%

--- lib/controllers/history_controller.dart
+++ lib/controllers/history_controller.dart
@@ -1,10 +1,13 @@
+import 'dart:convert';
 import 'dart:io';
 
+import 'package:dart_ipify/dart_ipify.dart';
 import 'package:device_info/device_info.dart';
 import 'package:get/get.dart';
 import 'package:network_info_plus/network_info_plus.dart';
 import 'package:wifi_info_flutter/wifi_info_flutter.dart';
 import 'package:wifi_speed_analyzer/models/wifi_speed.dart';
+import 'package:http/http.dart' as http;
 
 class HistoryController extends GetxController{
   WifiSpeedProvider wifiSpeedProvider = WifiSpeedProvider();
@@ -24,8 +27,10 @@ class HistoryController extends GetxController{
 
   void insertWifiSpeed(double downloadSpeed, double uploadSpeed) async {
     var wifiName = await info.getWifiName(); // FooNetwo
-    var ipAddress = await info.getWifiIP();
+    var ipAddress = await Ipify.ipv4();
     var device = '';
+    var server = '';
+    var isp = '';
 
     try {
       DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
@@ -40,14 +45,37 @@ class HistoryController extends GetxController{
     } catch(e){
       print(e);
     }
+
+    try{
+      var wifiInfo = await getWifiInfo(ipAddress);
+      print(wifiInfo);
+      server = "${wifiInfo['country']}, ${wifiInfo['countryCode']}";
+      isp = wifiInfo['isp'];
+    }catch(e){
+      print(e);
+    }
+
     WifiSpeed wifiSpeed = WifiSpeed(
       downloadSpeed: downloadSpeed,
       uploadSpeed: uploadSpeed,
       name: wifiName,
       ipAddress: ipAddress,
       device: device,
+      server: server,
+      isp: isp,
     );
     wifiSpeed = await wifiSpeedProvider.insert(wifiSpeed);
     wifiSpeeds.add(wifiSpeed);
+  }
+
+  Future<dynamic> getWifiInfo(String ipAddress) async {
+    final response = await http.get(Uri.parse('http://ip-api.com/json/$ipAddress'));
+
+    if (response.statusCode != 200) {
+      throw Exception('error');
+    }
+
+    final objJson = jsonDecode(response.body);
+    return objJson;
   }
 }
--- lib/pages/HistoryDetailPage.dart
+++ lib/pages/HistoryDetailPage.dart
@@ -5,9 +5,14 @@ import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:wifi_speed_analyzer/controllers/history_controller.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:wifi_speed_analyzer/models/wifi_speed.dart';
 import 'package:wifi_speed_analyzer/theme/color_theme.dart';
 
-class HistoryPage extends StatelessWidget{
+class HistoryDetailPage extends StatelessWidget{
+  final WifiSpeed wifiSpeed;
+
+  const HistoryDetailPage({Key key, this.wifiSpeed}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     final HistoryController historyController = Get.find(tag: 'history');
@@ -26,80 +31,11 @@ class HistoryPage extends StatelessWidget{
         ),
         backgroundColor: backgroundColor,
       ),
-      body: Obx(() => SingleChildScrollView(
-        child: Column(
+      body: Center(
+        child: Obx(() => Column(
           children: [
-            SizedBox(
-              height: 10,
-            ),
             for (var wifiSpeed in historyController.wifiSpeeds)
-              Padding(
-                padding: EdgeInsets.all(18.sp),
-                child: Container(
-                  height: 120.h,
-                  decoration: BoxDecoration(
-                      color: Color(0xff181818),
-                      borderRadius: BorderRadius.circular(8.sp),
-                      border: Border.all(color: Colors.blueAccent)
-                  ),
-                  child: Column(
-                    children: [
-                      Padding(
-                        padding: EdgeInsets.all(16.sp),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceAround,
-                          children: [
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text('Date'.toUpperCase()),
-                                Text(wifiSpeed.createdAt.toIso8601String()),
-                                Text(''),
-                              ],
-                            ),
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text('Download'.toUpperCase()),
-                                Text(wifiSpeed.downloadSpeed.toStringAsFixed(2)),
-                                Text('Mbps'),
-                              ],
-                            ),
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text('Upload'.toUpperCase()),
-                                Text(wifiSpeed.uploadSpeed.toStringAsFixed(2)),
-                                Text('Mbps'),
-                              ],
-                            ),
-                          ],
-                        ),
-                      ),
-                      Spacer(),
-                      Container(
-                        padding: EdgeInsets.symmetric(horizontal: 16.sp),
-                        height: 25.h,
-                        decoration: BoxDecoration(
-                          color: mainColor,
-                          borderRadius: BorderRadius.only(
-                            bottomLeft: Radius.circular(8.sp),
-                            bottomRight: Radius.circular(8.sp),
-                          ),
-                        ),
-                        child: SizedBox(
-                            width: MediaQuery.of(context).size.width,
-                            child: Align(
-                              alignment: Alignment.centerLeft,
-                              child: Text('WIFI NAME: ${wifiSpeed.isp.toString()}'),
-                            )
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              )
-              /*Row(
+              Row(
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
                   Column(
@@ -199,10 +135,10 @@ class HistoryPage extends StatelessWidget{
                     ],
                   ),
                 ],
-              ),*/
+              ),
           ],
-        ),
-      )),
+        )),
+      ),
     );
   }
 
